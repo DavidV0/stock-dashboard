8ab308b3b91f7f4e7ffb9ce03142d5bb5d4fa87e
YourGitHubUsername <davidvelickovic@hotmail.com>
2024-10-22 13:19:53 +0200

    first component created

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
-	<div id="app"></div>
+	<BaseCard>
+		<h1>Title</h1>
+		<p>This is content inside the card component.</p>
+	</BaseCard>
+
+	<BaseCard>
+		<p>Another card with different content.</p>
+	</BaseCard>
 </template>
 
 <script>
+import BaseCard from './components/BaseCard.vue';
+
 export default {
 	name: 'App',
-	components: {},
+	components: {
+		BaseCard,
+	},
 };
 </script>
 
@@ -14,11 +25,13 @@ body {
 	margin: 0;
 }
 #app {
+	padding: 100px;
 	background: radial-gradient(
 		71.11% 100% at 50% 0%,
 		#020204 14.6%,
 		#011f35 100%
 	);
+	box-sizing: border-box;
 	width: 100vw;
 	height: 100vh;
 }
